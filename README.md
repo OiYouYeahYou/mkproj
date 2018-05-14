7ebc8013e964c2fc5dee069a13d9f60b4ad7b18b
Jason <github@jallan.co.uk>
2018-05-14 23:17:45 +0100

    wip

--- README.md
+++ README.md
@@ -23,3 +23,27 @@
   * [x] * Any other packages
 * [ ] init commit
 * [ ] Publish to gh
+
+_prepublish_: Run BEFORE the package is packed and published, as well as on local npm install without any arguments. (See below)
+_prepare_: Run both BEFORE the package is packed and published, and on local npm install without any arguments (See below). This is run AFTER prepublish, but BEFORE prepublishOnly.
+_prepublishOnly_: Run BEFORE the package is prepared and packed, ONLY on npm publish. (See below.)
+_prepack_: run BEFORE a tarball is packed (on npm pack, npm publish, and when installing git dependencies)
+_postpack_: Run AFTER the tarball has been generated and moved to its final destination.
+_publish, postpublish_: Run AFTER the package is published.
+_preinstall_: Run BEFORE the package is installed
+_install, postinstall_: Run AFTER the package is installed.
+_preuninstall, uninstall_: Run BEFORE the package is uninstalled.
+_postuninstall_: Run AFTER the package is uninstalled.
+_preversion_: Run BEFORE bumping the package version.
+_version_: Run AFTER bumping the package version, but BEFORE commit.
+_postversion_: Run AFTER bumping the package version, and AFTER commit.
+_pretest, test, posttest_: Run by the npm test command.
+_prestop, stop, poststop_: Run by the npm stop command.
+_prestart, start, poststart_: Run by the npm start command.
+_prerestart, restart, postrestart_: Run by the npm restart command. Note: npm restart will run the stop and start scripts if no restart script is provided.
+_preshrinkwrap, shrinkwrap, postshrinkwrap_: Run by the npm shrinkwrap command.
+
+lint
+postinstall
+start
+prestart - gulp
--- src/index.ts
+++ src/index.ts
@@ -1,38 +1,216 @@
 #!/usr/bin/env node
+require('source-map-support').install()
+
 import co from 'co'
-import * as prompt from 'co-prompt'
+import * as coPrompt from 'co-prompt'
 import * as program from 'commander'
-import { existsSync, mkdirSync, writeFileSync } from 'fs'
+import { existsSync, mkdirSync, writeFileSync, readFileSync } from 'fs'
 import { join } from 'path'
 import git = require('simple-git/promise')
 import { resolve } from 'dns'
-import { spawnSync } from 'child_process'
+import { execSync } from 'child_process'
+import { compile } from 'handlebars'
+
+const ownPackage = require('../package.json')
+
+const rc = {
+	verbose: true,
+	publish: false,
+	commit: false,
+	info: 'This is temporary description',
+	license: 'MIT',
+	user: 'OiYouYeahYou',
+}
 
 program
+	.version(ownPackage.version)
 	.arguments('<file>')
-	.option('-d, --dry', "Make config, mut don't act")
+	.option('-D, --dry', "Make config, mut don't act")
+	.option('-v, --verbose', 'Verbosity')
+	.option('-p, --publish', 'Publish')
+	.option('-c, --verbose', 'commit')
 	.option('-i, --indent <indent>', 'Indentation to use')
+	.option('-n, --name <project-name>', '')
+	.option('-d, --description <info>', '')
+	.option('-l, --license <license>', '')
 	.parse(process.argv)
 
 co(function*() {
-	const name = yield prompt('Project name: ')
-	const target = join(process.cwd(), name)
-	const exists = existsSync(target)
+	const config: config = yield makeConfig()
 
-	if (exists) {
-		console.log('folder already exists')
-		return process.exit()
+	if (!program.dry) yield act(config)
+	else console.log(config)
+})
+	.catch(e => {
+		console.log(e)
+	})
+	.then(() => process.exit())
+
+function makeConfig(): Promise<config> {
+	return co(function*() {
+		const name = yield prompt('project-name', nameValidator)
+		const license = yield prompt('license')
+		const user = yield prompt('user')
+		const description = yield prompt('info')
+		const dependencies = yield prompt('dependencies')
+		const publish = yield boolPrompt('publish')
+		const commit = yield boolPrompt('commit')
+
+		const target = join(process.cwd(), name)
+
+		const pkg = pkgMaker(user, name, description, license)
+		const config: config = {
+			name,
+			license,
+			target,
+			description,
+			pkg,
+			publish,
+			commit,
+			dependencies,
+			user,
+		}
+
+		return config
+	})
+}
+
+function nameValidator(value): validationinfo {
+	if (typeof value !== 'string') return [false, 'is not a string']
+	if (!value.length) return [false, 'is blank']
+	const target = join(process.cwd(), value)
+	const exists = existsSync(target)
+	return [!exists, 'folder already exists']
+}
+
+async function act(config: config) {
+	const { target, license, name, description, pkg, dependencies } = config
+
+	log(`making directory: ${target}`)
+	mkdirSync(target)
+	mkdirSync(target + 'src')
+	process.chdir(target)
+	const repo = git(target)
+	// @ts-ignore untyped method
+	await repo.init()
+
+	// "npm init"
+	writeJSON(join(target, 'package.json'), pkg, 2)
+	writeJSON(join(target, '.mkproj'), config)
+
+	makefile('README.md', '.', config)
+	makefile('.prettierrc', '.', config)
+	makefile('.gitignore', '.', config)
+
+	const npm = 'npm i' + (dependencies ? ` && npm i ${dependencies}` : '')
+	safeExec(npm, 'failed to npm install')
+
+	safeExec(`code ${target}`, 'failed to open VS Code')
+	safeExec(`github open ${target}`, 'failed to open github')
+}
+
+function writeJSON(filename: string, data: any, indent?: number | '\t') {
+	const space = indent || program.indent || '\t'
+	const json = JSON.stringify(data, undefined, space)
+	log(`Writting ${filename}`)
+	writeFileSync(filename, json)
+}
+
+function safeExec(command: string, errorMessage: string) {
+	log(`calling ${command}`)
+	try {
+		execSync(command, { stdio: 'ignore' })
+	} catch (e) {
+		console.log(errorMessage)
+		log(e)
+	}
+}
+
+type validationFn = (value: string) => validationinfo
+type validationinfo = [boolean, string]
+
+function prompt(key: string, validator?: validationFn): Promise<string> {
+	return co(function*() {
+		const [isByArg, isByRC, preValue] = checkPreExistingData(key)
+
+		if (isByArg) {
+			return preValue
+		}
+
+		while (true) {
+			const defaultInfo = isByRC ? ` [${preValue}]` : ''
+			const promptResult = yield coPrompt(`${key}${defaultInfo}: `)
+			const value = processPromptValue(promptResult, isByRC, preValue)
+
+			if (callValidator(validator, value)) return value
+		}
+	})
+}
+
+function callValidator(validator: validationFn, value: string): boolean {
+	if (typeof validator !== 'function') return true
+	const [valid, message] = validator(value)
+	if (!valid) console.log(message)
+	return valid
+}
+
+function processPromptValue(
+	value: string,
+	present: boolean,
+	preExistingValue: string
+) {
+	if (value) return value
+	return present ? preExistingValue : ''
+}
+
+function* boolPrompt(key: string) {
+	const [present, pre] = checkPreExistingData(key)
+	if (present) return pre
+
+	const yes = ['y', 'yes']
+	const no = ['n', 'no']
+
+	while (true) {
+		const input = (yield coPrompt(key + ' (y/n):')).toLowerCase()
+		if (yes.indexOf(input) !== -1) return true
+		if (no.indexOf(input) !== -1) return false
+	}
+}
+
+function checkPreExistingData(key: string): [boolean, boolean, any] {
+	if (key in program && typeof program[key] !== 'string') {
+		return [true, false, program[key]]
 	}
 
-	const license = yield prompt('license: ')
-	const user = yield prompt('user: ')
-	const description = yield prompt('description: ')
-	const dependencies = yield prompt('dependencies: ')
+	if (key in rc) {
+		return [true, true, rc[key]]
+	}
 
-	const pkg = {
+	return [false, false, null]
+}
+
+function makefile(sourceName: string, destination?: string, config?: config) {
+	const sourcePath = join(__dirname, '../assets', sourceName)
+	const destinationPath = join(process.cwd(), destination || '', sourceName)
+
+	const content = readFileSync(sourcePath).toString()
+	const result = compile(content)(config)
+
+	log(`writing ${sourcePath} to ${destinationPath}`)
+	writeFileSync(destinationPath, result)
+}
+
+function pkgMaker(
+	user: string,
+	name: string,
+	description: string,
+	license: string
+) {
+	return {
 		name,
 		description,
 		license,
+
 		version: '0.0.0',
 		main: 'lib/index.js',
 		script: {
@@ -60,65 +238,10 @@ co(function*() {
 			typescript: '^2.8.3',
 		},
 	}
-
-	const publishResponse = yield prompt('publish: ')
-	const publish = publishResponse == 'y' || publishResponse == 'yes'
-	const commitResponse = yield prompt('commit: ')
-	const commit = commitResponse == 'y' || commitResponse == 'yes'
-
-	const config: config = {
-		name,
-		license,
-		target,
-		description,
-		pkg,
-		publish,
-		commit,
-		dependencies,
-		user,
-	}
-
-	if (!program.dry) yield act(config)
-	else console.log(config)
-
-	process.exit()
-})
-
-function* act(config: config) {
-	const { target, license, name, description, pkg, dependencies } = config
-
-	mkdirSync(target)
-	process.chdir(target)
-	const repo = git(target)
-	// @ts-ignore untyped method
-	yield repo.init()
-
-	// "npm init"
-	writeJSON(join(target, 'package.json'), pkg)
-
-	safeSpawn(`github open ${target}`, 'failed to open github')
-	safeSpawn('npm i', 'failed to install')
-
-	if (dependencies)
-		safeSpawn(
-			`npm i ${dependencies}`,
-			'failed to install additional dependencies'
-		)
-
-	writeJSON(join(target, '.mkproj'), config)
 }
 
-function writeJSON(filename: string, data: any) {
-	const json = JSON.stringify(data, undefined, program.indent || '\t')
-	writeFileSync(filename, json)
-}
-
-function safeSpawn(command: string, errorMessage: string) {
-	try {
-		spawnSync(command)
-	} catch (e) {
-		console.log(errorMessage)
-	}
+function log(...params: any[]) {
+	if (rc.verbose || program.verbose) console.log(...params)
 }
 
 interface config {
